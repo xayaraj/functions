4a91d1ab02bd03c42120e2bcdc4a9d6313c3ab58
Ian Stokes-Rees <ijstokes@continuum.io>
2014-10-06 11:44:57 -0400

    added docstrings

--- functions.py
+++ functions.py
@@ -1,6 +1,9 @@
+" Module demonstrating different ways to create function-like things in Python"
+
 __author__ = 'ijstokes'
 
 def adder(a, b):
+    " simple demonstration of function to perform addition"
     return a + b
 
 adder_lambda = lambda a, b: a + b
